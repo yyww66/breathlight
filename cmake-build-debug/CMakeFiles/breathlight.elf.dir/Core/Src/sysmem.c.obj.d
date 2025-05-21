932861b5a8b873b130182443804b1021c4f2cee3
洋 吴 <awsd1523awsd15231@outlook.com>
2025-05-21 13:02:00 +0800

    串口呼吸灯

---
+++ cmake-build-debug/CMakeFiles/breathlight.elf.dir/Core/Src/sysmem.c.obj.d
@@ -0,0 +1,21 @@
+CMakeFiles/breathlight.elf.dir/Core/Src/sysmem.c.obj: \
+ D:\Program\ Files\STM32CubeMX\breathlight\Core\Src\sysmem.c \
+ D:/toolchain/arm-none-eabi/13.3\ rel1/arm-none-eabi/include/errno.h \
+ D:/toolchain/arm-none-eabi/13.3\ rel1/arm-none-eabi/include/sys/errno.h \
+ D:/toolchain/arm-none-eabi/13.3\ rel1/arm-none-eabi/include/sys/reent.h \
+ D:/toolchain/arm-none-eabi/13.3\ rel1/arm-none-eabi/include/_ansi.h \
+ D:/toolchain/arm-none-eabi/13.3\ rel1/arm-none-eabi/include/newlib.h \
+ D:/toolchain/arm-none-eabi/13.3\ rel1/arm-none-eabi/include/_newlib_version.h \
+ D:/toolchain/arm-none-eabi/13.3\ rel1/arm-none-eabi/include/sys/config.h \
+ D:/toolchain/arm-none-eabi/13.3\ rel1/arm-none-eabi/include/machine/ieeefp.h \
+ D:/toolchain/arm-none-eabi/13.3\ rel1/arm-none-eabi/include/sys/features.h \
+ D:/toolchain/arm-none-eabi/13.3\ rel1/lib/gcc/arm-none-eabi/13.3.1/include/stddef.h \
+ D:/toolchain/arm-none-eabi/13.3\ rel1/arm-none-eabi/include/sys/cdefs.h \
+ D:/toolchain/arm-none-eabi/13.3\ rel1/arm-none-eabi/include/machine/_default_types.h \
+ D:/toolchain/arm-none-eabi/13.3\ rel1/arm-none-eabi/include/sys/_types.h \
+ D:/toolchain/arm-none-eabi/13.3\ rel1/arm-none-eabi/include/machine/_types.h \
+ D:/toolchain/arm-none-eabi/13.3\ rel1/arm-none-eabi/include/sys/lock.h \
+ D:/toolchain/arm-none-eabi/13.3\ rel1/lib/gcc/arm-none-eabi/13.3.1/include/stdint.h \
+ D:/toolchain/arm-none-eabi/13.3\ rel1/arm-none-eabi/include/stdint.h \
+ D:/toolchain/arm-none-eabi/13.3\ rel1/arm-none-eabi/include/sys/_intsup.h \
+ D:/toolchain/arm-none-eabi/13.3\ rel1/arm-none-eabi/include/sys/_stdint.h
---
+++ cmake-build-debug/CMakeFiles/breathlight.elf.dir/DependInfo.cmake
@@ -0,0 +1,67 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/Program Files/STM32CubeMX/breathlight/Core/Startup/startup_stm32f103c8tx.s" "D:/Program Files/STM32CubeMX/breathlight/cmake-build-debug/CMakeFiles/breathlight.elf.dir/Core/Startup/startup_stm32f103c8tx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "D:/Program Files/STM32CubeMX/breathlight/Core/Inc"
+  "D:/Program Files/STM32CubeMX/breathlight/Drivers/STM32F1xx_HAL_Driver/Inc"
+  "D:/Program Files/STM32CubeMX/breathlight/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "D:/Program Files/STM32CubeMX/breathlight/Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "D:/Program Files/STM32CubeMX/breathlight/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Program Files/STM32CubeMX/breathlight/Core/Src/gpio.c" "CMakeFiles/breathlight.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Core/Src/gpio.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Core/Src/main.c" "CMakeFiles/breathlight.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Core/Src/main.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Core/Src/stm32f1xx_hal_msp.c" "CMakeFiles/breathlight.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Core/Src/stm32f1xx_it.c" "CMakeFiles/breathlight.elf.dir/Core/Src/stm32f1xx_it.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Core/Src/stm32f1xx_it.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Core/Src/syscalls.c" "CMakeFiles/breathlight.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Core/Src/syscalls.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Core/Src/sysmem.c" "CMakeFiles/breathlight.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Core/Src/sysmem.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Core/Src/system_stm32f1xx.c" "CMakeFiles/breathlight.elf.dir/Core/Src/system_stm32f1xx.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Core/Src/system_stm32f1xx.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Core/Src/tim.c" "CMakeFiles/breathlight.elf.dir/Core/Src/tim.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Core/Src/tim.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Core/Src/usart.c" "CMakeFiles/breathlight.elf.dir/Core/Src/usart.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Core/Src/usart.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj.d"
+  "D:/Program Files/STM32CubeMX/breathlight/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj" "gcc" "CMakeFiles/breathlight.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
